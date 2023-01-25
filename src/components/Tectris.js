d6144a161228e432182862a5557c1d0692733891
BARarch <pebblebeachprogramming@gmail.com>
2023-01-25 00:59:42 -0500

    :tada hoks started

--- src/components/Tectris.js
+++ src/components/Tectris.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState }  from "react";
 
-import { createStage } from '../gamehelpers'
+// Styled Components
 import { StyledTectrisWrapper, StyledTectris } from './styles/StyledTectris'
+
+// Custom Hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage'
 
 // Components
 import Stage from './Stage';
@@ -9,17 +13,27 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tectris = () => {
- 
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player)
+
+    console.log('re-render')
     return (
         <StyledTectrisWrapper>
             <StyledTectris>
-                <Stage stage={ createStage() } />
+                <Stage stage={stage} />
                 <aside>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over"/>
+                    ) : (
                     <div>
                         <Display text="Score" />
                         <Display text="Rows" />
                         <Display text="Level" />    
                     </div>
+                    )}
                     <StartButton />
                 </aside>
             </StyledTectris>
