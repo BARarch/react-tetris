ec4133cf6de98f5638abf38d34c644147d2ce082
BARarch <pebblebeachprogramming@gmail.com>
2023-02-17 22:05:37 -0500

    :tada: shape rotation

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react';
-import { STAGE_WIDTH } from '../gamehelpers';
+import { checkCollision, STAGE_WIDTH } from '../gamehelpers';
 
 import { TETROMINOS, randomTetromino } from '../tetrominus';
 
@@ -9,6 +9,31 @@ export const usePlayer = () => {
         tetromino: TETROMINOS[0].shape,
         collided: false
     }); 
+
+    const rotate = (matrix, dir) => {
+        const rotatedMatrix = matrix.map((_, index) => matrix.map((col) => col[index])); // Transpose
+        if (dir > 0) return rotatedMatrix.map((row) => row.reverse());  // Then flip, 1 for CW -1 for CCW 
+        return rotatedMatrix.reverse();
+    };
+
+    const playerRotate = (stage, dir) => {
+        const clonedPlayer = JSON.parse(JSON.stringify(player));
+        clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+        const pos = clonedPlayer.pos.x;
+        let offset = 1;
+        while(checkCollision(clonedPlayer, stage, { x:0, y:0 })) {
+            clonedPlayer.pos.x += offset;
+            offset = -(offset + (offset > 0 ? 1 : -1)); // Takes offset though 1 -> -2 -> 3 -> -4 ...
+            if (offset > clonedPlayer.tetromino[0].length) {
+                rotate(clonedPlayer.tetromino, -dir);
+                clonedPlayer.pos.x = pos;
+                return;
+            } 
+        }
+    
+        setPlayer(clonedPlayer)
+    };
     
     const updatePlayerPos = ({ x, y, collided }) => {
         console.log(`x:${x} y${y}`)
@@ -18,7 +43,7 @@ export const usePlayer = () => {
             collided: collided,
 
         }))
-    }
+    };
 
     const resetPlayer = useCallback(() => {
         console.log("reset-player")
@@ -29,5 +54,5 @@ export const usePlayer = () => {
         })
     }, []);
 
-    return [player, updatePlayerPos, resetPlayer];
+    return [player, updatePlayerPos, resetPlayer, playerRotate];
 };
